5778b60d2a641cf5ca4debb198f6d17e2d01cf30
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2023-03-07 15:40:27 +0100

    Adding logo to HomeComponent.

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,8 +1,18 @@
+import Image from 'next/image';
 import styles from '@/styles/Header.module.css';
 
 export default function HeaderComponent() {
   return (
     <header className={styles.header}>
+      <Image
+        src="/favicon-invert.png"
+        alt="API Logo"
+        className={styles.logo}
+        width={25}
+        height={25}
+        title="What is IT - API"
+        priority
+      />
       <span>
         <b>What is IT - API</b> /&nbsp;
         <small>
