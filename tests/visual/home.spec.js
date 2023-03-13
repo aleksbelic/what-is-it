872e1dad3ef311117290f4bcd1fdfff6781f2519
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2023-03-13 23:15:04 +0100

    Adding screenshot for Linux comparison.

--- tests/visual/home.spec.js
+++ tests/visual/home.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test');
 
 test('home page looks ok', async ({page}) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('/');
   await expect(page).toHaveScreenshot();
 });
