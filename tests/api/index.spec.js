f1a283a852561dda8359f9a8c17b8a5e326271b0
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2023-03-22 21:18:25 +0100

    Removes Playwright, adds Cypress.

--- tests/api/index.spec.js
+++
@@ -1,8 +0,0 @@
-import {test, expect} from '@playwright/test';
-
-test('should return welcome msg for /api endpoint', async ({request}) => {
-  const indexResponse = await request.get('/api');
-  expect(await indexResponse.json()).toStrictEqual({
-    message: 'Welcome to What is IT - API',
-  });
-});
--- tests/visual/home.spec.js
+++
@@ -1,6 +0,0 @@
-import {test, expect} from '@playwright/test';
-
-test('home page looks ok', async ({page}) => {
-  await page.goto('/');
-  await expect(page).toHaveScreenshot();
-});
